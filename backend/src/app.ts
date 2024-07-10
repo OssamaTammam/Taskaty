18cd263ef31b881bf3bd9fcc88352016a9950d4e
Ossama Tammam <ossamatammam02@gmail.com>
2024-07-10 20:15:31 +0300

    implement password hashing and verification logic

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,18 @@
 import express, { Express } from "express";
+import morgan from "morgan";
 
 const app: Express = express();
+
+// Development Middleware
+
+// Put data in the request body
+app.use(express.json());
+
+// Logging requests
+app.use(morgan("dev"));
+
+// Routing
+const apiVersion: string = "1";
+const apiPrefix: string = `api/v${apiVersion}`;
 
 export default app;
