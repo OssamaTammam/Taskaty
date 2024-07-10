cfdd6cf3714af3d818c2376b7f5606528f81cbed
Ossama Tammam <ossamatammam02@gmail.com>
2024-07-11 00:59:25 +0300

    Backend 9: Interface to specify the type of data the user needs while signin up/logging in

--- backend/src/model/userModel.ts
+++ backend/src/model/userModel.ts
@@ -2,6 +2,15 @@ import { User } from "@prisma/client";
 import prisma from "../utils/prismaClient";
 
 /**
+ * Interface to specify a type of the user data used during signing up process
+ */
+export interface UserData {
+  username: string;
+  password: string;
+  email: string;
+}
+
+/**
  *  Helper function to check if the password is of length 8, has one symbol, one capital letter and one number at least
  *
  * @param password
@@ -20,7 +29,6 @@ const isPasswordValid = (password: string): boolean => {
  * @param email
  * @returns boolean if the email is valid
  */
-
 export const checkEmailSpecification = (email: string): boolean => {
   // Regex pattern to validate email address
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -35,7 +43,6 @@ export const checkEmailSpecification = (email: string): boolean => {
  * @param password
  * @returns boolean if the entered data is using the right formats
  */
-
 export const checkUsernamePasswordSpecification = (
   username: string,
   password: string,
@@ -44,4 +51,40 @@ export const checkUsernamePasswordSpecification = (
   if (!isPasswordValid(password)) return false;
 
   return true;
+};
+
+/**
+ * @param email
+ * @param username
+ * @returns boolean that checks if the user exists as a registered user in the database
+ */
+export const checkUserExits = async (
+  email: string,
+  username: string,
+): Promise<boolean> => {
+  try {
+    // check if username exists
+    if (
+      await prisma.user.findFirst({
+        where: {
+          username: username,
+        },
+      })
+    )
+      return true;
+
+    // check if email exists
+    if (
+      await prisma.user.findFirst({
+        where: {
+          email: email,
+        },
+      })
+    )
+      return true;
+
+    return false;
+  } catch (err) {
+    throw new Error("Error checking if user exits\n" + err);
+  }
 };
